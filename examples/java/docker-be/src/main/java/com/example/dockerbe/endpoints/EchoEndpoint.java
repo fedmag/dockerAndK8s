9e8790e02d945248df958c4733d80d8c0a35f737
gerfem <maggiorefederico@outlook.com>
2023-11-30 16:06:06 +0100

    added working example of communication between fe and be through docker-compose

--- examples/java/docker-be/src/main/java/com/example/dockerbe/endpoints/EchoEndpoint.java
+++ examples/java/docker-be/src/main/java/com/example/dockerbe/endpoints/EchoEndpoint.java
@@ -1,11 +1,13 @@
 package com.example.dockerbe.endpoints;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost", maxAge = 3600)
 @RequestMapping("echo")
 public class EchoEndpoint {
     
